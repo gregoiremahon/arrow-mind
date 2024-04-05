53b7d60897a9e88fca2bac8f8569ccdf3d5c68db
gregoiremahon <greg.mahondu78@gmail.com>
2024-04-05 17:48:36 +0200

    Logique du jeu implémentée en BAREMETAL

--- src/FreeRTOS/Demo/ARM7_LPC1769_Keil_RVDS/main.c
+++ src/FreeRTOS/Demo/ARM7_LPC1769_Keil_RVDS/main.c
@@ -10,6 +10,8 @@
 #define PIN_WR   (1 << 11) // Write - P0.11
 #define PIN_RD   (1 << 13) // Read - P2.13
 
+#define P0_MASK(bit) (1 << bit) // Macro pour créer un masque pour le port 0
+
 // Définition des masques pour les broches de données D0-D7 sur le port 2
 #define LCD_DATA_MASK (0xFF) // D0-D7 sur P2.0 à P2.7
 
@@ -28,13 +30,6 @@
 #define GRAY1		0x8410
 #define GRAY2		0x4208
 
-// Lecture Bitmap
-extern const unsigned char image_palette[256 * 3];
-
-extern const uint16_t imageData[];
-extern const int imageWidth;
-extern const int imageHeight;
-
 // Directions possibles des flèches à l'écran.
 typedef enum {
     UP,
@@ -42,6 +37,61 @@ typedef enum {
     LEFT,
     RIGHT
 } ArrowDirection;
+
+// ports des boutons en fonction de leurs directions (sur le port P0.x)
+
+#define UP_BUTTON    6  // Bleu
+#define DOWN_BUTTON  7  // Noir
+#define LEFT_BUTTON  9  // Rouge
+#define RIGHT_BUTTON 8  // Blanc
+
+// Séquence de directions 
+#define MAX_SEQUENCE 10 // Longueur maximale de la séquence
+ArrowDirection sequence[MAX_SEQUENCE];
+int sequenceLength = 0; // Longueur actuelle de la séquence
+
+// Lecture Bitmap
+extern const unsigned char image_palette[256 * 3];
+
+extern const uint16_t imageData[];
+extern const int imageWidth;
+extern const int imageHeight;
+
+
+// Générer une séquence de directions
+
+void generateSequence(int level) {
+		int i;
+    sequenceLength = level; // Augmente avec le niveau
+    for (i = 0; i < sequenceLength; i++) {
+        sequence[i] = (ArrowDirection)(rand() % 4); // Génère une direction aléatoire et la convertit en ArrowDirection
+    }
+}
+
+// Initialiser le port du bouton comme entrée
+void initButtonPort(uint8_t portBit) {
+    LPC_GPIO0->FIODIR &= ~P0_MASK(portBit); // Configure le pin spécifié comme entrée
+}
+
+void initButtonPorts(void) {
+    initButtonPort(6); // Initialiser le port du bouton BLEU (P0.6)
+    initButtonPort(7); // Initialiser le port du bouton NOIR (P0.7)
+    initButtonPort(8); // Initialiser le port du bouton BLANC (P0.8)
+    initButtonPort(9); // Initialiser le port du bouton ROUGE (P0.9)
+}
+
+// Lire l'état du bouton sur P0.x (x=portBit)
+int readButtonState(uint8_t portBit) {
+	
+		/*
+		Rouge : P0.9
+		Blanc : P0.8
+		Noir : P0.7
+		Bleu : P0.6
+		*/
+		 return (LPC_GPIO0->FIOPIN & P0_MASK(portBit)) ? 0 : 1;
+}
+
 
 void initGPIO(void) {
     LPC_GPIO0->FIODIR |= PIN_RST | PIN_CS | PIN_DC | PIN_WR;
@@ -211,6 +261,8 @@ void TFTinit(void) {
 }
 
 /*void fillScreen(uint16_t color) {
+		// Fonction pour remplir l'écran d'une couleur unie (rempli pas en entier; a revoir)
+
 		int i; 
     // Sélectionnez la zone d'écran entière
     sendCMD(0x2A); // Commande pour colonne (X)
@@ -296,8 +348,16 @@ void displayArrow(const uint16_t *imageData, int x, int y, int width, int height
     int i, j;
     
     setArea(x, y, width, height);
-
+	
+		// INVERSION DES DIRECTIONS CAR ECRAN A L'ENVERS
     switch (direction) {
+        case UP: direction = DOWN; break;
+        case DOWN: direction = UP; break;
+        case LEFT: direction = RIGHT; break;
+        case RIGHT: direction = LEFT; break;
+    }    
+		
+		switch (direction) {
         case UP:
             // Orientation originale, pas de rotation
             for (i = 0; i < height; i++) {
@@ -336,11 +396,18 @@ void displayArrow(const uint16_t *imageData, int x, int y, int width, int height
     }
 }
 
-void displayMultipleArrows(const uint16_t *imageData, int width, int height) {
+void displayMultipleArrows(const uint16_t *imageData, int width, int height, int times) {
+		/* 
+		Fonction pour afficher quatre flèches tournantes dans la même direction 
+		Affiche times fois la rotation (peut être utile par exemple durant l'initialisation du jeu -> 10 times = 1 seconde environ)
+		*/
+	
     int delay;
     ArrowDirection currentDirection = UP; 
-
-    while (1) {
+		int counter = 0;
+	
+    while (counter < times) {
+				// Léger décalage d'affichage car les affichages sont appelés un par un pour chaque flèche.
         displayArrow(imageData, 0, 40, width, height, currentDirection);
         displayArrow(imageData, 120, 40, width, height, currentDirection); 
         displayArrow(imageData, 0, 160, width, height, currentDirection); 
@@ -362,6 +429,7 @@ void displayMultipleArrows(const uint16_t *imageData, int width, int height) {
                 currentDirection = UP;
                 break;
         }
+				counter++;
     }
 }
 
@@ -383,52 +451,177 @@ void fillScreen(uint32_t color) {
     LPC_GPIO0->FIOSET = PIN_CS; // Désélectionne le TFT
 }
 
-int main(void) {
-		uint64_t i;
+void displaySequence(void) {
+		int i;
+		volatile int delay;
+    for (i = 0; i < sequenceLength; i++) {
+        displayArrow(up_image_data, 120, 40, 120, 120, sequence[i]);
+        for (delay = 0; delay < 1000000; delay++); // Délai entre les flèches
+    }
+}
+
+void addDirectionToSequence(ArrowDirection newDirection) {
+    if (sequenceLength < MAX_SEQUENCE) {
+        sequence[sequenceLength++] = newDirection;
+    }
+}
+
+int readPlayerInput(void) {
+    int currentInput;
+    int inputIndex = 0; // Index pour suivre les entrées de l'utilisateur
+		int i;
+    volatile int delay;
+    int inputSequence[MAX_SEQUENCE]; // Tableau pour stocker les entrées de l'utilisateur
+
+    // Réinitialise le tableau des entrées
+    for(i = 0; i < MAX_SEQUENCE; i++) {
+        inputSequence[i] = -1;
+    }
+
+
+    while (inputIndex < sequenceLength) {
+        currentInput = -1;
+
+        // Lecture de l'entrée de l'utilisateur
+        if (readButtonState(UP_BUTTON) == 1) {
+            currentInput = UP;
+        } else if (readButtonState(DOWN_BUTTON) == 1) {
+            currentInput = DOWN;
+        } else if (readButtonState(LEFT_BUTTON) == 1) {
+            currentInput = LEFT;
+        } else if (readButtonState(RIGHT_BUTTON) == 1) {
+            currentInput = RIGHT;
+        }
+
+        // Si une entrée a été détectée
+        if (currentInput != -1) {
+            // Attendre un court instant pour le debouncing
+            for (delay = 0; delay < 50000; delay++);
+
+            // Confirmation de l'entrée
+            if ((currentInput == UP && readButtonState(UP_BUTTON) == 0) ||
+                (currentInput == DOWN && readButtonState(DOWN_BUTTON) == 0) ||
+                (currentInput == LEFT && readButtonState(LEFT_BUTTON) == 0) ||
+                (currentInput == RIGHT && readButtonState(RIGHT_BUTTON) == 0)) {
+
+                // Stocke l'entrée de l'utilisateur dans le tableau
+                inputSequence[inputIndex++] = currentInput;
+                
+                // Vérifie si la séquence d'entrée correspond à la séquence attendue jusqu'à présent
+                for (i = 0; i < inputIndex; i++) {
+                    if (inputSequence[i] != sequence[i]) {
+                        return 0; // Échec si l'entrée ne correspond pas à la séquence attendue
+                    }
+                }
+
+                // Affichage visuel de la direction entrée
+                displayArrow(up_image_data, 0, 40, 120, 120, currentInput);
+            }
+        }
+    }
+
+    return 1; // Succès si toute la séquence est correctement saisie
+}
+
+
+
+
+
+ArrowDirection generateNextDirection(ArrowDirection lastDirection) {
+    ArrowDirection nextDirection;
+    do {
+        nextDirection = (ArrowDirection)(rand() % 4); // Génère une nouvelle direction aléatoirement
+    } while (nextDirection == lastDirection); // Répète si la nouvelle direction est identique à la dernière
+    return nextDirection;
+}
+
+void initGame(void) {
     SystemInit(); // Initialisation du système
     TFTinit(); // Initialisation de l'écran TFT
+		initButtonPorts(); // Initialiser les ports des boutons
 		
-	/* COLOR SHIFT TEST
-    fillScreen(RED); // Remplissage de l'écran en rouge
-		for (i = 0; i < 5000000; i++);
-		fillScreen(CYAN);
-		for (i = 0; i < 5000000; i++);
-		fillScreen(BLUE);
-		for (i = 0; i < 5000000; i++);
-		fillScreen(GRAY2);
-		for (i = 0; i < 5000000; i++);
-		fillScreen(YELLOW);
-		for (i = 0; i < 5000000; i++);
-		fillScreen(BLACK);
-		for (i = 0; i < 5000000; i++);
-		fillScreen(YELLOW);
-		fillScreen(RED);
-	*/
-		/*// Affichage de l'image BMP en mode UP LEFT DOWN RIGHT LEFT
-		for (i = 0; i < 5000000; i++);
-		//displayImage(up_image_data, 0, 0, up_image_width, up_image_width);
-		displayArrow(up_image_data, 0, 0, up_image_width, up_image_height, UP); // pas de rotation
-		for (i = 0; i < 5000000; i++);
-		for (i = 0; i < 5000000; i++);
-		displayArrow(up_image_data, 0, 120, up_image_width, up_image_height, LEFT); // s'affiche a gauche de la premiere image
-		for (i = 0; i < 5000000; i++);
-		for (i = 0; i < 5000000; i++);
-		displayArrow(up_image_data, 120, 0, up_image_width, up_image_height, LEFT); // s'affiche a gauche de la premiere image
-		for (i = 0; i < 5000000; i++);
-		for (i = 0; i < 5000000; i++);
-		displayArrow(up_image_data, 120, 0, up_image_width, up_image_height, LEFT); // s'affiche a gauche de la premiere image
-		for (i = 0; i < 5000000; i++);
-		for (i = 0; i < 5000000; i++);
-		displayArrow(up_image_data, 0, 0, up_image_width, up_image_height, DOWN); 
-		for (i = 0; i < 5000000; i++);
-		for (i = 0; i < 5000000; i++);
-		displayArrow(up_image_data, 0, 0, up_image_width, up_image_height, RIGHT); 
-		for (i = 0; i < 5000000; i++);
-		for (i = 0; i < 5000000; i++);
-		displayArrow(up_image_data, 0, 0, up_image_width, up_image_height, LEFT); */
-		displayMultipleArrows(up_image_data, 120,120);
+		// Définir manuellement la première direction
+    //addDirectionToSequence(UP); 
+    //srand(time(NULL)); // Initialisation du générateur de nombres aléatoires
+}
+
+void restartGame(void) {
+    initGame(); 
+    sequenceLength = 0;
+    //addDirectionToSequence(UP);
+}
+
+void playGame(void) {
+    int level = 1;
+    initGame();
+		displayMultipleArrows(up_image_data, 120,120, 5);
+		TFTinit();
+
 
     while (1) {
-        // Boucle infinie
+        displaySequence(); // Affichez la séquence à l'écran
+
+        if (readPlayerInput()) { // Vérifiez si l'entrée du joueur correspond à la séquence
+						// Assurez-vous que la dernière direction de la séquence est correctement obtenue
+						ArrowDirection lastDirection = sequenceLength > 0 ? sequence[sequenceLength - 1] : (ArrowDirection)-1;
+						
+						// Génère une nouvelle direction différente de la dernière direction de la séquence
+						ArrowDirection newDirection = generateNextDirection(lastDirection);
+						
+						// Ajoute cette nouvelle direction à la séquence pour le prochain niveau
+						addDirectionToSequence(newDirection);
+						level++; // Augmentez le niveau
+        } else {
+            // Le joueur a échoué, fin du jeu
+						displayMultipleArrows(up_image_data, 120,120, 3);
+						restartGame();
+						playGame();
+            break;
+        }
     }
+}
+
+/*
+FOnction de jeu avec quatre niveaux directions prédéfinies : haut bas gauche droite
+void playGame(void) {
+    int level = 1; // Commence au niveau 1
+    initGame();
+    displayMultipleArrows(up_image_data, 120, 120, 5);
+    TFTinit();
+
+    // Réinitialisez la longueur de la séquence à chaque début de jeu
+    sequenceLength = 0;
+
+    while (level <= 4) {
+        // Ajouter une nouvelle direction à la séquence pour chaque niveau
+        switch (level) {
+            case 1:
+                addDirectionToSequence(UP);
+                break;
+            case 2:
+                addDirectionToSequence(DOWN);
+                break;
+            case 3:
+                addDirectionToSequence(LEFT);
+                break;
+            case 4:
+                addDirectionToSequence(RIGHT);
+                break;
+        }
+
+        displaySequence(); // Affichez la séquence à l'écran
+        if (readPlayerInput()) { // Vérifiez si l'entrée du joueur correspond à la séquence
+            level++; // Augmentez le niveau
+        } else {
+            // Le joueur a échoué, fin du jeu
+            //displayMultipleArrows(up_image_data, 120, 120, 20);
+            break;
+        }
+    }
+}
+*/
+
+int main(void) {
+    playGame();
+    return 0;
 }
